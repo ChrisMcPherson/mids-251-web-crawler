9b8bfcb51a182ed94a48fefff1a18c6e941c5b76
ChrisMcPherson <summernightdrive@gmail.com>
2016-12-03 22:02:35 -0500

    Updated connection

--- source/web_crawler/mongodb_query.py
+++ source/web_crawler/mongodb_query.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-#root:C3Y7lDPL@
+
+# database: cdsearchdb      collection: webpages
 client = MongoClient('mongodb://169.45.131.5/cdsearchdb')
-#client = MongoClient()
 db = client.cdsearchdb
 
-#{"raw_url": "randomURLhash1"}
-cursor = db.webpages.find()
+all_docs_cursor = db.webpages.find()
 
-for document in cursor:
+for document in all_docs_cursor:
     print(document)
     break
+
